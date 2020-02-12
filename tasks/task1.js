06c133b140bb3ce540330f941893bc3b711d125c
Михайленко Татьяна <t.mihaileco@reg.ru>
2020-02-12 17:11:46 +0200

    test

---
+++ tasks/task1.js
@@ -0,0 +1,9 @@
+let x = 2, y = 1;
+
+if (x < y) {
+	console.log('x is less than y');
+} else if (x > y) {
+	console.log('x is greater than y');
+} else if (x == y) {
+	console.log('x equals y');
+}
---
+++ tasks/task2.js
@@ -0,0 +1,5 @@
+let fruitList = ['apple', 'watermelon', 'lemon', 'orange', 'kiwi'];
+
+fruitList.forEach((item, index) => {
+  console.log(`${index}: ${item}`);
+});
